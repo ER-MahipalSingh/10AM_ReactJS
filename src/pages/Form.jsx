ead7f2f59a37088faa45b26aef2c89fba9fafd3c
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-07-03 11:01:20 +0530

    Add files via upload

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -5,14 +5,39 @@ const Form = () => {
     email: "",
     password: "",
     city: "surat",
+    skills: [],
   });
+  const [error, setError] = useState({});
 
   const changeInputHanlder = (e) => {
-    setFromData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      const addSkills = checked
+        ? [...formData.skills, value]
+        : formData.skills.filter((skill) => skill !== value);
+      setFromData({ ...formData, skills: addSkills });
+    } else {
+      setFromData({ ...formData, [name]: value });
+    }
+  };
+
+  const formValidation = () => {
+    let error = {};
+
+    if (formData.email.trim() === "") {
+      error.email = "Email is require";
+    }
+
+    setError(error);
+    return Object.keys(error).length === 0;
   };
 
   const formHandler = (e) => {
     e.preventDefault();
+    if (!formValidation()) return;
+
+    
     console.log(formData);
   };
 
@@ -37,6 +62,7 @@ const Form = () => {
             placeholder="Enter your name"
             name="password"
             onChange={changeInputHanlder}
+            value={formData.password}
           />
         </div>
 
@@ -49,8 +75,46 @@ const Form = () => {
           </select>
         </div>
 
+        <div>
+          <label>Skills</label>
+          <div>
+            <label htmlFor="html">HTML</label>
+            <input
+              type="checkbox"
+              name="skills"
+              id="html"
+              value="html"
+              onChange={changeInputHanlder}
+              checked={formData.skills.includes("html")}
+            />
+          </div>
+          <div>
+            <label htmlFor="css">CSS</label>
+            <input
+              type="checkbox"
+              name="skills"
+              id="css"
+              value="css"
+              onChange={changeInputHanlder}
+              checked={formData.skills.includes("css")}
+            />
+          </div>
+          <div>
+            <label htmlFor="js">JS</label>
+            <input
+              type="checkbox"
+              name="skills"
+              id="js"
+              value="js"
+              onChange={changeInputHanlder}
+              checked={formData.skills.includes("js")}
+            />
+          </div>
+        </div>
+
         <button type="submit">Submit</button>
       </form>
+      <p>{error.email}</p>
     </div>
   );
 };
